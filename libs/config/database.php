e415081cda1af9d69f435945c3467268d56b7b16
duonghuupha <duonghuupha@hotmail.com.vn>
2025-07-22 16:39:28 +0700

    Database

--- libs/config/database.php
+++ libs/config/database.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'edusoft');
+define('DB_NAME', 'sonthuy');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 ?>
