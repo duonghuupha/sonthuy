358865d4f4396253dc912fece8c485d41b17bd8e
duonghuupha <duonghuupha@hotmail.com.vn>
2025-08-21 01:46:31 +0700

    36

---
+++ views/one_true/form.php
@@ -0,0 +1,68 @@
+<h5 class="mb-3">Danh sách đáp án (tối đa 4)</h5>
+
+<!-- Vòng lặp 4 đáp án -->
+<div class="row g-3">
+    <!-- Đáp án 1 -->
+    <div class="col-12 col-md-6">
+        <div class="card p-3">
+            <div class="form-check mb-2">
+                <input class="form-check-input" type="radio" name="correct" value="0" required>
+                <label class="form-check-label">Đáp án đúng</label>
+            </div>
+            <div class="mb-2">
+                <input type="text" class="form-control" name="answer_text_0" placeholder="Nội dung đáp án" required>
+            </div>
+            <div>
+                <input type="file" class="form-control" name="answer_image_0" accept="image/*">
+            </div>
+        </div>
+    </div>
+
+    <!-- Đáp án 2 -->
+    <div class="col-12 col-md-6">
+        <div class="card p-3">
+            <div class="form-check mb-2">
+                <input class="form-check-input" type="radio" name="correct" value="1">
+                <label class="form-check-label">Đáp án đúng</label>
+            </div>
+            <div class="mb-2">
+                <input type="text" class="form-control" name="answer_text_1" placeholder="Nội dung đáp án" required>
+            </div>
+            <div>
+                <input type="file" class="form-control" name="answer_image_1" accept="image/*">
+            </div>
+        </div>
+    </div>
+
+    <!-- Đáp án 3 -->
+    <div class="col-12 col-md-6">
+        <div class="card p-3">
+            <div class="form-check mb-2">
+                <input class="form-check-input" type="radio" name="correct" value="2">
+                <label class="form-check-label">Đáp án đúng</label>
+            </div>
+            <div class="mb-2">
+                <input type="text" class="form-control" name="answer_text_2" placeholder="Nội dung đáp án">
+            </div>
+            <div>
+                <input type="file" class="form-control" name="answer_image_2" accept="image/*">
+            </div>
+        </div>
+    </div>
+
+    <!-- Đáp án 4 -->
+    <div class="col-12 col-md-6">
+        <div class="card p-3">
+            <div class="form-check mb-2">
+                <input class="form-check-input" type="radio" name="correct" value="3">
+                <label class="form-check-label">Đáp án đúng</label>
+            </div>
+            <div class="mb-2">
+                <input type="text" class="form-control" name="answer_text_3" placeholder="Nội dung đáp án">
+            </div>
+            <div>
+                <input type="file" class="form-control" name="answer_image_3" accept="image/*">
+            </div>
+        </div>
+    </div>
+</div>
